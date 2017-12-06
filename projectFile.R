086ef732df6aeb8b91ac6ef20de4eade36ac8648
nirmal nepal <rockink_nirmal@live.com>
2017-12-06 00:13:49 -0500

    added more r program for analysis

--- projectFile.R
+++ projectFile.R
@@ -1,6 +1,25 @@
-sales = read.csv("data.csv")
-attach(data)
+sales = read.csv("data.csv", na.strings=c("","N/A","tbd"))
 genre = sales[,1]
+Year[Year > 2017] = NA
+
+#attach the data frame
+attach(data)
+
+## Frequency table for # games released (by Year and by Platform)
+freqYearPlatform = table(Year,Platform)
+
+## Plot
+# Uncomment the next line for creating a pdf figure
+pdf("mygraph.pdf")
+year = sort(unique(Year))
+matplot(year, freqYearPlatform, type="l", col=1:6, lty=1:5,
+        xlab="Year Of Release", ylab="# Games")
+title("Game Releases Per Platform")	
+legend(1980,500,as.character(levels(Platform)),col=1:6,lty=1:5,cex=.5)
+# Uncomment the next line for creating a pdf figure
+dev.off()
+
+
 
 cor(sales["EU_Sales"], sales["Other_Sales"])
 cor(sales["EU_Sales"], sales["NA_Sales"])
@@ -59,28 +78,62 @@ allYears
 
 generateAllConsole <- function(allYears){
   consoleList = matrix(nrow = length(allYears), ncol = length(consoleName))
+  gameListforThatYear = sales[sales$Year == allYears[0], ]
+  
   for(i in 1:length(allYears)){
-    #for(i in 1:3){
-      gameListforThatYear = sales[sales$Year == allYears[i], ]
-      # hey but we just want consoles.. 
-      gameConsolesForThatYear = table(gameListforThatYear[,"Platform"])
-      gameListforThatYearInTable = table(gameConsolesForThatYear)
-      #print(gameConsolesForThatYear)
-      
-      # add them all
-      print(allYears[i]); 
-      print(gameConsolesForThatYear)
-      consoleList[i] = as.vector(gameConsolesForThatYear)
+  #for(i in 1:3){
+    gameListforThatYear = sales[sales$Year == allYears[i], ]
+    # hey but we just want consoles.. 
+    gameConsolesForThatYear = table(gameListforThatYear[,"Platform"])
+    gameListforThatYearInTable = table(gameConsolesForThatYear)
+    #print(gameConsolesForThatYear)
+    
+    consoleList[i,] = as.vector(gameConsolesForThatYear)
   }
   consoleList
 }
 
 consoleList = generateAllConsole(allYears = allYears)
+rownames(consoleList) = as.vector(allYears)
+colnames(consoleList) = as.vector(consoleName)
+
 consoleList
 
-
+barplot(consoleList[,c("DS","PS2")], col=c("darkblue", "red","yellow","green","orange"), legend=rownames(consoleList))
+legend("bottom", legend = consoleName, pch=10:19, horiz = TRUE)
 #show psp in graph
+
+#just ps2 and ds 
+matplot(tdData, type="b", pch = 10:19)
+legend("topleft", legend = c("DS","PS2"), pch=10:19)
+sum(tdData[,c("DS")]) #2163
+sum(tdData[,c("PS2")]) #2161
+
 barplot(table(Year[Platform == "PSP"]), col = rainbow(20))
 barplot(table(Year[Platform == "XB"]), col = rainbow(20))
 barplot(table(Year[Platform == "PS4"]), col = rainbow(20))
+
+
+# this calculates the number of consoles active 
+# in a year.. this is the answer to question 1.2b. 
+numConsoles = function(freqYearPlatform, allYears){
+  
+  consolesByYear = numeric(0)
+  len = length(allYears) - 1
+  for(i in 1:len){
+    #basic idea is to count if games sold is > 0, 0 meaning its absent
+    consolesByYear[toString(allYears[i])] = sum(freqYearPlatform[toString(allYears[i]),] > 0)
+  }
+  
+  consolesByYear
+}
+
+consoleByYear = numConsoles(freqYearPlatform, allYears)
+consoleByYear
+barplot(consoleByYear, col=rainbow(20), ylim = c(0,12))
+
+
+matplot(consoleByYear, freqYearPlatform, type="l", col=1:6, lty=1:5,
+        xlab="Num of active game sales by year", ylab="# Games")
+
 
